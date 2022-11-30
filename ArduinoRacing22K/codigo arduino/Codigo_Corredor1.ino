a268808524496199d635f3f9143f3e5d62b36c3a
Leguerrero97 <116394500+Leguerrero97@users.noreply.github.com>
2022-11-30 10:16:05 -0300

    Add files via upload

---
+++ ArduinoRacing22K/codigo arduino/Codigo_Corredor1.ino
@@ -0,0 +1,160 @@
+#include <SoftwareSerial.h>
+#include <Adafruit_NeoPixel.h>
+#define PIN        6  
+#define NUMPIXELS  151
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); //define el objeto pixels que es la tira de leds
+
+SoftwareSerial miBT1(10, 11);
+
+void setup()
+{
+  Serial.begin(9600);
+  pixels.begin();
+  Serial.println("Listo");
+  miBT1.begin(38400); //38400
+}
+
+void loop()
+{
+  if (miBT1.available())//Lee el moduloBT y envia a Arduino
+  {
+  Serial.write(miBT1.read());
+  }
+  int i = 0;
+  int cantidad_de_vueltas = 0;
+  int vueltas_hechas = 0;
+
+//CONFIGURACION DE VUELTAS///////////////////////////////////////////////////////////////////////////////////
+ if (miBT1.read() != -1 && miBT1.read() > 3 && miBT1.read() < 5)
+ {
+  cantidad_de_vueltas = 1;
+    Serial.println("1");
+    delay(3000);
+ }
+ 
+  if (miBT1.read() != -1 && miBT1.read() > 5 && miBT1.read() < 7)
+ {
+  cantidad_de_vueltas = 3;
+  Serial.println("3");
+    delay(3000);
+ }
+ 
+  if (miBT1.read() != -1 && miBT1.read() > 7 && miBT1.read() < 9)
+ {
+  cantidad_de_vueltas = 5;
+  Serial.println("5");
+  delay(3000);
+ }
+ 
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //// CÓDIGO PARA CUANDO SE HAN CONFIGURADO VUELTAS
+  //COMANDOS PARA AVANZAR
+  if(cantidad_de_vueltas != 0)
+  {
+    while(vueltas_hechas != cantidad_de_vueltas)
+    {
+         for(int a = 0; a < 2; a++)
+         {
+            if (miBT1.read() != -1)
+            {
+             Serial.println(miBT1.read());
+             pixels.setPixelColor(i - 2, pixels.Color(0,0,0));
+             pixels.setPixelColor(i - 1, pixels.Color(100,0,255));
+             pixels.setPixelColor(i, pixels.Color(100,0,255));
+             pixels.setPixelColor(i + 1, pixels.Color(100,0,255));
+             pixels.show();
+             i++;
+             }
+      
+            if(a == 1)
+            {
+            a = a-1;
+            }
+         
+            if(i == 152)
+            {
+             i = i-150;
+             vueltas_hechas++;
+             pixels.setPixelColor(151, pixels.Color(0,0,0));
+                for(int x = 0; x < 152; x++)
+                {
+                  if(x != i)
+                  {
+                    pixels.setPixelColor(x-2, pixels.Color(0,255,0));
+                    pixels.setPixelColor(x-1, pixels.Color(0,255,0));
+                    pixels.setPixelColor(x, pixels.Color(0,255,0));
+                    pixels.setPixelColor(x+1, pixels.Color(0,255,0));
+                    pixels.setPixelColor(x+2, pixels.Color(0,255,0));
+                    pixels.show();
+                    pixels.setPixelColor(x-2, pixels.Color(0,0,0));
+                    pixels.setPixelColor(x-1, pixels.Color(0,0,0));
+                    pixels.setPixelColor(x, pixels.Color(0,0,0));
+                    pixels.setPixelColor(x+1, pixels.Color(0,0,0));
+                    pixels.setPixelColor(x+2, pixels.Color(0,0,0));
+                    pixels.show();
+                  }
+                }   
+            }
+         }
+    }
+    if(vueltas_hechas == cantidad_de_vueltas)
+    {
+       for(int q = 0; q < 152; q++)
+                {
+                    pixels.setPixelColor(q, pixels.Color(255,0,255));
+                    pixels.show();
+                }   
+    }
+    
+  }
+  else
+  {
+
+        for(int a = 0; a < 2; a++)
+        {
+              if (miBT1.read() != -1)
+              {
+                 Serial.println(miBT1.read());
+                 pixels.setPixelColor(i - 2, pixels.Color(0,0,0));
+                 pixels.setPixelColor(i - 1, pixels.Color(255,0,0));
+                 pixels.setPixelColor(i, pixels.Color(255,0,0));
+                 pixels.setPixelColor(i + 1, pixels.Color(255,0,0));
+                 pixels.show();
+                 i++;
+              }
+  
+                if(a == 1)
+                {
+                  a = a-1;
+                }
+
+                if(i == 152)
+                {
+                  i = i-150;
+                  pixels.setPixelColor(151, pixels.Color(0,0,0));
+                      for(int x = 0; x < 152; x++)
+                      {
+                          if(x != i)
+                          {
+                            pixels.setPixelColor(x-2, pixels.Color(0,255,0));
+                            pixels.setPixelColor(x-1, pixels.Color(0,255,0));
+                            pixels.setPixelColor(x, pixels.Color(0,255,0));
+                            pixels.setPixelColor(x+1, pixels.Color(0,255,0));
+                           pixels.setPixelColor(x+2, pixels.Color(0,255,0));
+                           pixels.show();
+                           pixels.setPixelColor(x-2, pixels.Color(0,0,0));
+                           pixels.setPixelColor(x-1, pixels.Color(0,0,0));
+                           pixels.setPixelColor(x, pixels.Color(0,0,0));
+                           pixels.setPixelColor(x+1, pixels.Color(0,0,0));
+                           pixels.setPixelColor(x+2, pixels.Color(0,0,0));
+                           pixels.show();
+                          }
+                      }
+                }  
+      }
+    
+  }
+//TERMINA AVANZAR
+ if (Serial.available())
+ miBT1.write(Serial.read()); //Lee Arduino y envia al moduloBT
+}
